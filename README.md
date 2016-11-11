e1885dda5955887d01ca07d0cbdcd146944cdd49
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2016-11-11 15:01:46 +0100

    Basic methods for buildings

---
+++ README.md
@@ -0,0 +1,4 @@
+On The Hill
+===========
+Turn-based strategy/city-building game. Very early work in progress
+so nothing works yet.
--- buildings.py
+++ buildings.py
@@ -1,6 +1,5 @@
 
 class BuildingPrototype:
-
     def __init__(self):
         #TODO: better constructor
         self.name = 'Test building'
@@ -9,20 +8,23 @@ class BuildingPrototype:
         self.upkeep_cost = 10
         self.additional_effects = {'health' : 0, 'technology': 0, 'prestige': 0, 'food': 0, 'safety': 0}
 
-   # def build(self, x, y, state):
-   #      if not state.actions:
-   #          print('Nie możesz nic zrobić w tej turze')
-   #          return
-   #
-   #      if state.money < self.base_price:
-   #          print('Nie stać cię cebulaku xd')
-   #          return
-   #
-   #      state.money -= self.base_price
-   #      state.actions -= 1
-   #      state.buildings.append(self)
-   #      print('Zbudowałeś')
-   #      return
+    def can_be_built(self, map_tile, neighbors):
+        return True
+
+    def _modify_state_attributes(self, state, func):
+        # lambdas and reflection, fuck yeah
+        for effect in self.additional_effects:
+            setattr(state, effect, func(getattr(state, effect), self.additional_effects[effect]))
+
+    def on_build(self, state):
+        # add semi-permanent effects when building
+        self._modify_state_attributes(state, lambda x, y: x + y)
+
+    def on_destroy(self, state):
+        # remove semi-permanent effects when destroying
+        self._modify_state_attributes(state, lambda x, y: x - y)
+
+
 
 def get_initial_buildings():
     #TODO: make basic buildings and return a list of them
