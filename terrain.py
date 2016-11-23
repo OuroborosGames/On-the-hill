1958fad5f6d8c51fe15362ad3b4af5b11017e7a1
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2016-11-23 19:29:59 +0100

    Fix bugs

--- terrain.py
+++ terrain.py
@@ -13,7 +13,7 @@ class MapPrototype:
     def __init__(self, h, w):
         self.heigth = h
         self.width = w
-        self._map_internal = self._generate_map(h,w)
+        self._map_internal = self._generate_map(h, w)
 
     def add_building(self, building, x, y):
         if self._map_internal[x][y].building:
@@ -30,9 +30,9 @@ class MapPrototype:
     def get_neighbors(self, x, y):
         ret = []
         for i in range(-1, 1):
-            for j in range(-1,1):
+            for j in range(-1, 1):
                 if x+i >= 0 and y+j >= 0:
-                    ret.append(self.get_field_by_coordinates(x+i,y+j))
+                    ret.append(self.get_field_by_coordinates(x+i, y+j))
         return ret
 
     @classmethod
@@ -51,19 +51,19 @@ class SimplexNoiseMap(MapPrototype):
     @classmethod
     def _generate_map(cls, h, w):
         noise = SimplexNoise()
-        return [[cls.get_terrain_from_noise(noise.noise2(x ,y)) for x in range(w)] for y in range(h)]
+        return [[cls.get_terrain_from_noise(noise.noise2(x, y)) for x in range(w)] for y in range(h)]
     
     @classmethod
-    def get_terrain_from_noise(value):
-    if value < -0.5:
-        return water
-    elif value < 0:
-        return grass
-    elif value < 0.2:
-        return forest
-    elif value < 0.5:
-        return hills
-    return mountains
+    def get_terrain_from_noise(cls, value):
+        if value < -0.5:
+            return cls.water
+        elif value < 0:
+            return cls.grass
+        elif value < 0.2:
+            return cls.forest
+        elif value < 0.5:
+            return cls.hills
+        return cls.mountains
 
 
 # def get_terrain_from_noise(value):
