f192a30940936aba4e96b096f9aa119af101f8cd
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2016-11-29 17:34:29 +0100

    Allow hardcoded objects

--- load_data.py
+++ load_data.py
@@ -2,22 +2,38 @@ import json
 from collections import OrderedDict
 
 
+# those functions are used by the game to initialize its state
 def get_initial_buildings():
-    return parse_json_data('buildings.json')
+    return _parse_json_data('buildings.json') + _get_hardcoded_buildings()
 
 
 def get_basic_random_events():
-    return parse_json_data('events.json')
+    return _parse_json_data('events.json') + _get_hardcoded_events()
 
 
 def get_basic_special_actions():
-    return parse_json_data('actions.json')
+    return _parse_json_data('actions.json') + _get_hardcoded_actions()
 
 
-def parse_json_data(filename):
-    return dict_to_game_objects(json.load(filename, object_hook=OrderedDict))
+# those internal functions handle data files
+def _parse_json_data(filename):
+    return _dict_to_game_objects(json.load(filename, object_hook=OrderedDict))
 
 
-def dict_to_game_objects(data_dict):
+def _dict_to_game_objects(data_dict):
     # TODO: actually create game objects
-    pass
+    return []
+
+
+# those internal functions are used to define more complex objects
+# (basically, if you see no reasonable way of creating something with data files, create it here)
+def _get_hardcoded_buildings():
+    return []
+
+
+def _get_hardcoded_events():
+    return []
+
+
+def _get_hardcoded_actions():
+    return []
