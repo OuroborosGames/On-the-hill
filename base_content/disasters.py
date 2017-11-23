a367aba0ecf1fc9c83f4f28ac7308bd010929de4
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2017-11-23 21:15:47 +0100

    Famine event

--- base_content/disasters.py
+++ base_content/disasters.py
@@ -5,19 +5,32 @@ from oth_core.text_events import *
 
 
 def get_disasters():
-    return []
+    return [famine]
 
 
 class Disaster(ConditionalEvent):
 
-    def __init__(self, name, description, actions, stat, counterval, threshold):
+    def __init__(self, name, description, actions, stat, counterval):
         self.stat = stat
-        self.threshold = threshold
         counterstat = "disaster_" + stat
         # those events will fire when an associated counter reaches a certain value...
         super().__init__(name, description, actions, lambda state: counter_greater(state, counterstat, counterval))
         # ...and they'll reset the counter afterwards, so you won't get the same events each turn
         self.chain_unconditionally(lambda state: state.counter.reset(counterstat))
 
-    def _is_disaster_over(self, state):
-        return attr_greater(state, self.stat, self.threshold)
+famine = Disaster(
+    name="Famine",
+    description=
+    """The food shortages your city was suffering from have now turned into a disastrous
+    famine. Many people have already died of starvation and many more are barely surviving
+    without food. The ones who didn't lose all their strength are either protesting outside
+    of the city hall or just trying to steal something even remotely edible.""",
+    actions={
+        'Do nothing': lambda state: modify_state(state, {'safety': -5, 'health': -3, 'prestige': -2}),
+        'Implement food rationing': lambda state: modify_state(state, {'food': state.food/2, 'prestige': -3}),
+        'Buy as much as you can afford and give to the people': lambda state: modify_state(
+            state, {'food': min(state.population, state.money), 'money': -state.money})
+    },
+    stat='food',
+    counterval=3
+)
