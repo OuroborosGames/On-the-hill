006b259662c29eda91d7e39edfabaa576577829c
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2017-11-23 20:41:15 +0100

    Class for disaster events

--- base_content/disasters.py
+++ base_content/disasters.py
@@ -6,3 +6,18 @@ from oth_core.text_events import *
 
 def get_disasters():
     return []
+
+
+class Disaster(ConditionalEvent):
+
+    def __init__(self, name, description, actions, stat, counterval, threshold):
+        self.stat = stat
+        self.threshold = threshold
+        counterstat = "disaster_" + stat
+        # those events will fire when an associated counter reaches a certain value...
+        super().__init__(name, description, actions, lambda state: counter_greater(state, counterstat, counterval))
+        # ...and they'll reset the counter afterwards, so you won't get the same events each turn
+        self.chain_unconditionally(lambda state: state.counter.reset(counterstat))
+
+    def _is_disaster_over(self, state):
+        return attr_greater(state, self.stat, self.threshold)
